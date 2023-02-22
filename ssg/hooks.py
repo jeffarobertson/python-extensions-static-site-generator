cb29d86a45c59b154bc8e4f54322d1f60bb84bfb
jeffrobertson <jrobertson@motivhealth.com>
2023-02-22 09:10:22 -0700

    update hooks.py

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -6,3 +6,8 @@ def register(hook, order=0):
         return func
     
     return register_callback
+
+def event(hook, *args):
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
