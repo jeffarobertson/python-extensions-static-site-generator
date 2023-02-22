bd5e5910c10be48e2c99675c109480f2822ca4cb
jeffrobertson <jrobertson@motivhealth.com>
2023-02-22 08:36:02 -0700

    update ssg/extensions.py

---
+++ ssg/extensions.py
@@ -0,0 +1,14 @@
+import sys
+import importlib
+
+from pathlib import Path
+
+
+def load_module(directory, name):
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+def load_directory(directory):
+    for path in directory:
+        
