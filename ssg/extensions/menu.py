d256d2791af4365b75ddc216cc2dfa5f8f4be723
jeffrobertson <jrobertson@motivhealth.com>
2023-02-22 09:23:49 -0700

    update menu.py

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -6,3 +6,8 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
+
+    for path in source.rglob("*"):
+        for parser in list(filter(filter_function, original_list)):
+            if parser.valid_file_ext(path.suffix):
+                files.append(path)
