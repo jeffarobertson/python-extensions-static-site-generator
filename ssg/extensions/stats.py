133fb049d81f305e5fbe3820fef33deb05e0ce50
jeffrobertson <jrobertson@motivhealth.com>
2023-02-22 10:07:55 -0700

    update stats.py

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,18 @@
+import time
+
+from ssg import hooks
+
+start_time = None
+total_written = 0
+
+
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = time.time()
+
+
+@hooks.register("written")
+def written():
+    global total_written
+    total_written += 1
